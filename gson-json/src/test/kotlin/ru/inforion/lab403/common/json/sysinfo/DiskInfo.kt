1445a9fde48ca3aea7c8ea5c36fbd0e8ea12fc7d
Aleksandre <aleksandre.pichkhadze@scandiweb.com>
2023-01-15 21:17:49 +0400

    remove unnecessary tests

--- gson-json/src/test/kotlin/ru/inforion/lab403/common/json/sysinfo/DiskInfo.kt
+++
@@ -1,7 +0,0 @@
-package ru.inforion.lab403.common.json.sysinfo
-
-import oshi.hardware.HWDiskStore
-
-data class DiskInfo(val model: String, val serial: String) {
-    constructor(disk: HWDiskStore) : this(disk.model, disk.serial)
-}
